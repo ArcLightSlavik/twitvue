575f587776033bb868eb697411533d8d3080ddc8
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-10-05 02:17:37 +0300

    Initial commit

---
+++ src/assets/users.js
@@ -0,0 +1,43 @@
+export const users = [
+    {
+        "id": 1,
+        "username": "ArcLight_Slavik",
+        "firstName": "Slavik",
+        "lastName": "Leskiv",
+        "email": null,
+        "isAdmin": true,
+        "tweets": [
+            {"id": 1, "content": "Twitter"},
+            {"id": 2, "content": "TWITTER!!!!!!!!"}
+        ]
+    },
+    {
+        "id": 2,
+        "username": "ctrlshifti",
+        "firstName": "Kat",
+        "lastName": "Maddox",
+        "email": null,
+        "isAdmin": false,
+        "tweets": [
+            {"id": 1, "content": "my 2020 character development: i've stopped adding coke to my rum and coke"}
+        ]
+    },
+    {
+        "id": 3,
+        "username": "JakeWrightUK",
+        "firstName": "Jake",
+        "lastName": "Wright",
+        "email": null,
+        "isAdmin": false,
+        "tweets": []
+    },
+    {
+        "id": 4,
+        "username": "39daph",
+        "firstName": "daphne",
+        "lastName": "daphne",
+        "email": null,
+        "isAdmin": false,
+        "tweets": []
+    }
+];
